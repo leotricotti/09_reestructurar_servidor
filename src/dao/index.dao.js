394e26be67109fa3d2cfb34843303b476ea327ad
Leonardo Tricotti <tricottileo@gmail.com>
2023-09-14 17:32:07 -0300

    commit

---
+++ src/dao/index.dao.js
@@ -0,0 +1,3 @@
+import mongoCartsDao from "./mongo/carts.dao";
+import mongoProductsDao from "./mongo/products.dao";
+import mongoUsersDao from "./mongo/users.dao";
--- src/dao/mongo/carts.dao.js
+++ src/dao/mongo/carts.dao.js
@@ -1,4 +1,4 @@
-import cartsModel from "../models/carts.model.js";
+import cartsModel from "./models/carts.model.js";
 
 export default class Cart {
   // Método asyncrono para obtener todos los carritos
